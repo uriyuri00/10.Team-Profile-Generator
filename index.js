7c84ea7b90274ca83be170bac8f46dea5c59599a
DESKTOP-G800GM0\uriyu <uriyuri00@gmail.com>
2023-01-23 15:24:49 -0800

    push2

--- index.js
+++ index.js
@@ -6,30 +6,35 @@ const Intern = require(`./lib/Intern`);
 const inquirer = require("inquirer");
 const fs = require("fs");
 
-var employees = []
+let employees = []
 
 // User answered Manager questions.. we create Manager and save to list of employees
-var yuri = new Manager("Yuri", 1, "uriyuri00@gmail.com", 01);
+const yuri = new Manager("Yuri", 1, "uriyuri00@gmail.com", 206-565-4218);
 employees.push(yuri)
 
 // User selected to add Intern... we create Intern and save to list of employees
-var josh = new Intern("josh", 2, "josh@gmail.com", 02);
+const josh = new Intern("josh", 2, "josh@gmail.com", "UCLA");
 employees.push(josh)
 
-var Don = new Intern("", 3, "Don@mail.com", 03);
+const Don = new Intern("", 3, "Don@mail.com", "UW");
 employees.push(Don)
 
-var Lullu = new Engineer("Lullu", 4, "Lullu@mail.com", 04);
+const Lullu = new Engineer("Lullu", 4, "Lullu@mail.com", "github.com/Lullu");
 employees.push(Lullu)
 
-var Bob = new Engineer("Bob", 5, "Bob@gmail.com", 05);
+const Bob = new Engineer("Bob", 5, "Bob@gmail.com", "github.com/uriyuri00");
 employees.push(Bob)
 
 function createHTML(employees) {
     // Add html based on what employee type
     var html1 = `
-    <h1>My team</h1>
-    <main> `;
+    <head> 
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet"
+    		integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We"
+        	crossorigin="anonymous">
+    </head>
+    <body> 
+        <h1 class="display-3 $-color: $red-500">My team</h1>`;
 
     var html2 = "";
     for(var i = 0; i<employees.length; i++) {
@@ -37,18 +42,23 @@ function createHTML(employees) {
 
         // All employee types
         html2+= "Name:" + currentEmployee.getName();
+        html2+= "Id:" + currentEmployee.getId();
+        html2+= "email:" + currentEmployee.getEmail();
 
         // Specific to Employee type
         if(currentEmployee.getRole()=="Manager") {
-            html2+= "Office Number" + currentEmployee.getOfficeNumber();
+            html2+= `<div class="container-sm"> Office Number` + currentEmployee.getOfficeNumber() + `</div>`;
         }
         if(currentEmployee.getRole()=="Intern") {
-            html2+= "School" + currentEmployee.getSchool();
+            html2+= `<div class="container-sm"> School` + currentEmployee.getSchool() + `</div>`;
+        }
+        if(currentEmployee.getRole()=="Engineer") {
+            html2+= `<div class="container-sm"> Github` + currentEmployee.getGithub() + `</div>`;
         }
     }
 
     var html3 = `
-    </main> `;
+    </body> `;
     
     fs.writeFile(`dist/index.html`, html1+html2+html3, ()=>{});
 }
@@ -56,3 +66,4 @@ function createHTML(employees) {
 // Pass list of employees aka array
 createHTML(employees);
 console.log("Written HTML");
+
