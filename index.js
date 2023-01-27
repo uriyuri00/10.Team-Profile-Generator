3f089472a7bb76c559037ae073472e6738430e95
DESKTOP-G800GM0\uriyu <uriyuri00@gmail.com>
2023-01-26 23:06:31 -0800

    change funtions

--- index.js
+++ index.js
@@ -1,69 +1,170 @@
-const Employee = require(`./lib/Employee`);
 const Manager = require(`./lib/Manager`);
 const Engineer = require(`./lib/Engineer`);
 const Intern = require(`./lib/Intern`);
+const employee = require('./lib/employee');
 
 const inquirer = require("inquirer");
 const fs = require("fs");
+const employees = [];
 
-let employees = []
+function ManagerQuestions() {
+    console.log('Welcome to Team Profile Generator');
+    inquirer
+        .prompt([
+            {
+                name: "name",
+                type: "input",
+                message: "What is your name?",
+            },
+            {
+                name: "id",
+                type: "input",
+                message: "what is team manager's employee Id?",
+            },
+            {
+                name: "email",
+                type: "input",
+                message: "What is manager's email?",
+            },
+            {
+                name: "officeNumber",
+                type: "input",
+                message: "What is team manager's office number?",
+            }
+            ])
+        .then(function(answers){
+            const manager = new Manager(answers.name, answers.id, answers.email, answers.officeNumber );
+            employees.push(manager);
+            AddMember()
+        })
+};
+function EngineerQuestions() {
+    inquirer
+        .prompt([
+            {
+                name: "name",
+                type: "input",
+                message: "What is your name?",
+            },
+            {
+                name: "id",
+                type: "input",
+                message: "what is team manager's employee Id?",
+            },
+            {
+                name: "email",
+                type: "input",
+                message: "What is manager's email?",
+            },
+            {
+                name: "github",
+                type: "input",
+                message: "What is Engineer's Github Id?",
+            }
+            ])
+        .then(function(answers){
+            const engineer = new Engineer(answers.name, answers.id, answers.email, answers.github );
+            employees.push(engineer);
+            AddMember()
+        })
+};
+function InternQuestions() {
+    inquirer
+        .prompt([
+            {
+                name: "name",
+                type: "input",
+                message: "What is your name?",
+            },
+            {
+                name: "id",
+                type: "input",
+                message: "what is team manager's employee Id?",
+            },
+            {
+                name: "email",
+                type: "input",
+                message: "What is manager's email?",
+            },
+            {
+                name: "school",
+                type: "input",
+                message: "What is Intern's school?",
+            }
+            ])
+        .then(function(answers){
+            const intern = new Intern(answers.name, answers.id, answers.email, answers.school );
+            employees.push(intern);
+            AddMember()
+        })
+};
 
-// User answered Manager questions.. we create Manager and save to list of employees
-const yuri = new Manager("Yuri", 1, "uriyuri00@gmail.com", 206-565-4218);
-employees.push(yuri)
-
-// User selected to add Intern... we create Intern and save to list of employees
-const josh = new Intern("josh", 2, "josh@gmail.com", "UCLA");
-employees.push(josh)
-
-const Don = new Intern("", 3, "Don@mail.com", "UW");
-employees.push(Don)
-
-const Lullu = new Engineer("Lullu", 4, "Lullu@mail.com", "github.com/Lullu");
-employees.push(Lullu)
-
-const Bob = new Engineer("Bob", 5, "Bob@gmail.com", "github.com/uriyuri00");
-employees.push(Bob)
+function AddMember(){
+    inquirer
+        .prompt([
+            {
+                name: "AddTeamMember",
+                type: "list",
+                message: "Do you want to add team member of finish?",
+                choices: ["Add an Engineer", "Add an Intern", "Finish"],
+              },
+        ])
+        .then((answers) => {
+            if(answers.AddTeamMember  === "Add an Engineer"){
+                EngineerQuestions();
+            } else if(answers.AddTeamMember === "Add an Intern"){
+                InternQuestions();
+            } else {
+                createHTML(employees);
+            }
+        })
+};
 
 function createHTML(employees) {
-    // Add html based on what employee type
-    var html1 = `
-    <head> 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet"
-    		integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We"
-        	crossorigin="anonymous">
-    </head>
-    <body> 
-        <h1 class="display-3 $-color: $red-500">My team</h1>`;
-
-    var html2 = "";
-    for(var i = 0; i<employees.length; i++) {
-        var currentEmployee = employees[i];
-
-        // All employee types
-        html2+= "Name:" + currentEmployee.getName();
-        html2+= "Id:" + currentEmployee.getId();
-        html2+= "email:" + currentEmployee.getEmail();
-
-        // Specific to Employee type
-        if(currentEmployee.getRole()=="Manager") {
-            html2+= `<div class="container-sm"> Office Number` + currentEmployee.getOfficeNumber() + `</div>`;
-        }
-        if(currentEmployee.getRole()=="Intern") {
-            html2+= `<div class="container-sm"> School` + currentEmployee.getSchool() + `</div>`;
-        }
-        if(currentEmployee.getRole()=="Engineer") {
-            html2+= `<div class="container-sm"> Github` + currentEmployee.getGithub() + `</div>`;
-        }
-    }
-
-    var html3 = `
-    </body> `;
+    const teamMembers =  employees.map(employee => {
+                
+            return `
+            <div class="card col-3 rounded-3">
+                <div class="card-title text text-white" style="margin: 10px; background-color: rgb(109, 82, 184); padding: 10px;">
+                    <h4 class="name">${employee.getName()}</h4>
+                    <h5 class="job-title">${employee.getRole()}</h5>
+                </div>
+                <div class="card-text">
+                    <p>Employee ID: ${employee.getId()} </p>
+                    <p>Email: <a href='mailto:${employee.getEmail()}'>${employee.getEmail()}</a></p>
+                    <p>${employee.getRole()==="Intern" ? "School: " + employee.getSchool(): employee.getRole()==="Manager" ? "Office Number: " + employee.getOfficeNumber(): "GitHub: " + "<a href='https://github.com/" + employee.getGithub() + "' target='_blank'>" + employee.getGithub() + "</a>"} </p>
+                </div>
+            </div> `
+        });
     
-    fs.writeFile(`dist/index.html`, html1+html2+html3, ()=>{});
-}
+         
+    
 
-// Pass list of employees aka array
-createHTML(employees);
-console.log("Written HTML");
+    const Html = `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+            integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <link href="./style.css" rel="stylesheet">
+        <title>Team Profile</title>
+    </head>
+    <body>
+        <div class="container-fluid" style="background-color: rgb(51, 39, 83); padding: 30px;">
+            <h1 class="text-center" style="color: white;">My Team</h1>
+        </div>
+        <div class="container row text-center justify-content-between" style="margin: 100px;"">
+           ${teamMembers.join('')}
+        </div>
+    </body>
+    </html>`;
 
+
+   fs.writeFile('dist/index.html', Html, 'utf-8', ()=>{console.log('Success!');});
+};
+
+ManagerQuestions();
+// createHTML(employees);
