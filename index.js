81d29bb111e3b6d04583a64729b6a1af7601eed4
DESKTOP-G800GM0\uriyu <uriyuri00@gmail.com>
2023-01-26 23:50:20 -0800

    css

--- index.js
+++ index.js
@@ -126,8 +126,8 @@ function createHTML(employees) {
             return `
             <div class="card col-3 rounded-3" style="background-color: rgb(218, 198, 226);">
                 <div style="margin: 10px; background-color: rgb(109, 82, 184); padding: 10px;">
-                    <h4>${employee.getName()}</h4>
-                    <h5>${employee.getRole()}</h5>
+                    <h2>${employee.getName()}</h2>
+                    <h3>${employee.getRole()}</h3>
                 </div>
                 <div class="card-text">
                     <p>Employee ID: ${employee.getId()} </p>
